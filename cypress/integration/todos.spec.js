797e752ffad2ddbd8ac005050e285968573f038d
Gabriel Giro Resende <gabriel.giro.resende@outlook.com>
2019-06-16 12:04:25 -0300

    Learn how to debug in cypress

--- cypress/integration/todos.spec.js
+++ cypress/integration/todos.spec.js
@@ -1,14 +1,22 @@
 describe('Todo Application', () => {
     it('loads the page', () => {
+
+        // Cypress schedule all tasks to do in the future [ visit, get, get ];
         cy.visit('/')
 
-        cy.get('.todo-list li:nth-child(1)')
+        cy.log('I will debug this element')
+        cy.get('[data-cy=todo-item-3]')
+          .then((element) => { debugger; })
           .should('have.text', 'Hello world')
           .should('not.have.class', 'completed')
           .find('.toggle')
           .should('not.be.checked')
 
-        cy.get('.todo-list li:nth-child(2)')
+        // This will pause the test running flow
+        debugger;
+
+        cy.get('[data-cy=todo-item-4]')
+          .debug()
           .should('have.text', 'Goodnight moon')
           .should('have.class', 'completed')
           .find('.toggle')

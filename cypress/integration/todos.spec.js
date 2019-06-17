42ed784ef0e302a2b0f98e00a356ee05db4af092
Gabriel Giro Resende <gabriel.giro.resende@outlook.com>
2019-06-16 22:21:08 -0300

    First cypress stub

--- cypress/integration/todos.spec.js
+++ cypress/integration/todos.spec.js
@@ -1,8 +1,24 @@
 describe('Todo Application', () => {
     it('loads the page', () => {
+        cy.server();
+
+        cy.route('/api/todos', [
+          {
+            "text": "Hello world",
+            "completed": false,
+            "id": 3
+          },
+          {
+            "id": 4,
+            "completed": true,
+            "text": "Goodnight moon"
+          }
+        ]).as('preload')
 
         // Cypress schedule all tasks to do in the future [ visit, get, get ];
         cy.visit('/')
+
+        cy.wait('@preload')
 
         cy.log('I will debug this element')
         cy.get('[data-cy=todo-item-3]')

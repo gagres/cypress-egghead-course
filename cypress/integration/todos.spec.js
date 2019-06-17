78ffe47367ad65f66e5784c40b743b9400cc0554
Gabriel Giro Resende <gabriel.giro.resende@outlook.com>
2019-06-16 23:18:34 -0300

    Added fixtures to the project

--- cypress/integration/todos.spec.js
+++ cypress/integration/todos.spec.js
@@ -1,29 +1,20 @@
 describe('Todo Application', () => {
+  beforeEach(() => {
+    cy.fixture('todos/all.json').as('todosPreload')
+  })
+
   it('loads the page', function () {
     cy.server()
 
-    let todos = [
-      {
-        "id": 1,
-        "text": "Hello world",
-        "completed": false
-      },
-      {
-        "id": 2,
-        "text": "Goodnight moon",
-        "completed": true
-      }
-    ]
-
     // Alias the fixture data
-    cy.route('/api/todos', todos).as('preload')
+    cy.route('/api/todos', '@todosPreload').as('preload')
 
     cy.visit('/')
     cy.wait('@preload')
 
     cy.store('example.test.first').should('equal', 1)
 
-    cy.store('todos').should('deep.equal', todos)
+    cy.store('todos').should('deep.equal', this.todosPreload)
 
     cy.get('[data-cy=todo-item-1]')
       .should('have.text', 'Hello world')

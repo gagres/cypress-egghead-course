9c182dd80ed22d8a516c0885cf7e392db6a72b24
Gabriel Giro Resende <gabriel.giro.resende@outlook.com>
2019-06-16 22:35:06 -0300

    Stub redux store (this is so cool)

--- cypress/integration/todos.spec.js
+++ cypress/integration/todos.spec.js
@@ -1,17 +1,33 @@
 describe('Todo Application', () => {
-    it('loads the page', () => {
-        cy.visit('/')
+  it('loads the page', () => {
+    cy.visit('/')
 
-        cy.get('[data-cy=todo-item-3]')
-          .should('have.text', 'Hello world')
-          .should('not.have.class', 'completed')
-          .find('.toggle')
-          .should('not.be.checked')
-
-        cy.get('[data-cy=todo-item-4]')
-          .should('have.text', 'Goodnight moon')
-          .should('have.class', 'completed')
-          .find('.toggle')
-          .should('be.checked')
+    cy.window().its('store').invoke('getState').should('deep.equal', {
+      todos: [
+        {
+          "text": "Hello world",
+          "completed": false,
+          "id": 3
+        },
+        {
+          "id": 4,
+          "completed": true,
+          "text": "Goodnight moon"
+        }
+      ],
+      visibilityFilter: 'show_all'
     })
+
+    cy.get('[data-cy=todo-item-3]')
+      .should('have.text', 'Hello world')
+      .should('not.have.class', 'completed')
+      .find('.toggle')
+      .should('not.be.checked')
+
+    cy.get('[data-cy=todo-item-4]')
+      .should('have.text', 'Goodnight moon')
+      .should('have.class', 'completed')
+      .find('.toggle')
+      .should('be.checked')
+  })
 })
